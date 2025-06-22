ec145c51a20067c61e9146d7b7a74e1638bee748
ForusOne <shins777@gmail.com>
2025-06-23 00:42:46 +0900

    update

--- adk/10-unittest/stock_REST.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=GOOG&interval=5min&apikey=CBPHIO7CGA890ZMA'
-r = requests.get(url)
-data = r.json()
-
-print(data)
